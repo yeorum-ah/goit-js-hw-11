763ebb3ee307a7ad16c0a620d6554a76d4d24033
Pangpang <vicleeua@gmail.com>
2023-07-27 19:47:10 +0900

    trial 1

---
+++ src/js/pic-api.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38477015-061959e62709d04c081e794b4';
+const searchParams = new URLSearchParams({
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+  per_page: 40,
+});
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async getImage() {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${searchParams}&page=${this.page}`
+    );
+    this.page += 1;
+
+    return response.data;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
